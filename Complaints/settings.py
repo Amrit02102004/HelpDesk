f902ee3bfe040df9413fa61761b7ef09de674c9c
Amrit Sundarka <114827768+Amrit02102004@users.noreply.github.com>
2024-01-28 12:46:03 +0530

    Update settings.py

--- Complaints/settings.py
+++ Complaints/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cwlz24#p$0n1^@xm0bnkjl(rb*297xoon10qqfbkuco#b&kulk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
